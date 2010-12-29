cf717ed18f06f4010f66749a29e4f7e197c55731
Benton Porter <benton.porter@gmail.com>
2010-12-28 18:59:03 -0600

    New Rails project (skeleton)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsContactForm::Application.config.secret_token = 'a4866ee27a9d7fa7b4da0b03f0e5d65f7ead9ecdd7b36a3992ffc178c5a60698ee279f36272adc6d6f7e7e24d77ac5810d4c669cca96a24b17d0412f56e279f1'
