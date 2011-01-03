f268466dd7bfdac8f484478137eadef14f16027b
Benton Porter <benton.porter@gmail.com>
2011-01-03 11:13:19 -0600

    Initial contact form implementation

--- app/models/contact_message.rb
+++ app/models/contact_message.rb
@@ -1,6 +1,8 @@
 class ContactMessage
+  # TODO - Why is :id needed?
   attr_accessor :id, :email, :subject, :body
   
+  # TODO - Why is this coded the way it is?  Is it necessary?
   def initialize(attributes = {})
     attributes.each do |key, value|
       send("#{key}=", value)
