c6d612437e8cbe526fc661dda621817cc552a723
Benton Porter <benton.porter@gmail.com>
2011-01-12 18:49:43 -0600

    Remove unnecessary comments

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,9 +1,7 @@
 class Notifier < ActionMailer::Base
   def contact_me(contact_message)
-    # Pass the message to the notifier view
     @contact_message = contact_message
     
-    # Return a new mail message
     mail(:to => App.email.address,
          :from => contact_message.email,
          :subject => contact_message.subject)
