342684f420aeb8d4eefb3fce20d686a2ea540558
Benton Porter <benton.porter@gmail.com>
2011-01-12 21:49:32 -0600

    Cleanup ContactMessage model

--- app/models/contact_message.rb
+++ app/models/contact_message.rb
@@ -1,13 +1,10 @@
 class ContactMessage
-  # TODO - Why is :id needed?
-  attr_accessor :id, :email, :subject, :body
+  attr_accessor :email, :subject, :body
   
-  # TODO - Why is this coded the way it is?  Is it necessary?
   def initialize(attributes = {})
     attributes.each do |key, value|
       send("#{key}=", value)
     end
-    @attributes = attributes
   end
   
   def save
